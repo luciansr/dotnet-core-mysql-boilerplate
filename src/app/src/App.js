35746e9bee7c7b5ffc8450b3d27016d47547abe8
Lucian Sturião <luciansr@gmail.com>
2018-12-11 23:39:58 -0200

    creating navbar and jumbotron

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,27 +1,13 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './home/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Button>TESTE</Button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Home></Home>
       </div>
     );
   }
